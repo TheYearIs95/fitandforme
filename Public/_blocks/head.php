e73a271263875e6657c6de0f5867e9dcc907d27c
TheYearIs95 <bmessaoud.firas@gmail.com>
2024-12-03 17:52:33 +0100

    Réparation menu burger JS et ajout des commentaires

--- Public/_blocks/head.php
+++ Public/_blocks/head.php
@@ -4,5 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="/Public/css/styles.css">
-<link rel="icon" type="image/x-icon" href="/public/img/icon/favicon-32x32.png">
+    <meta name="description" content="Fit and Forme est une association sportive à but non lucratif
+                    dédiée à la pratique du fitness, de la danse et du renforcement musculaire.">
+    <link rel="stylesheet" href="/Public/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="/public/img/icon/favicon-32x32.png">
+
+
+
+
+    
--- Public/js/burger.js
+++ Public/js/burger.js
@@ -1,8 +1,13 @@
 // #################################### Code menu burger ######################################
-burger = document.querySelector(".burger-icon");
+const burger = document.querySelector(".burger-icon");
 burger.onclick = function (e) {
-    console.log(e);
     e.stopPropagation();
-    navBar = document.querySelector(".nav");
+    const navBar = document.querySelector(".nav");
     navBar.classList.toggle("active");
 }
+
+
+
+
+
+
